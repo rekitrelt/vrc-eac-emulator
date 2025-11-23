daf44db3973ec733471242dbbf084c6a5df378b9
rekitrelt <gagexg@icloud.com>
2025-11-22 21:14:58 -0800

    simple crashlog fix

--- src/common/exception_handler.hpp
+++ src/common/exception_handler.hpp
@@ -1,27 +1,22 @@
 #pragma once
 
-#include <crashlog/parser.h>
+#include <windows.h>
 #include <winuser.h>
 
 #include <sstream>
 
 class exception_handler {
-	static LONG WINAPI TopLevelExceptionFilter(struct _EXCEPTION_POINTERS *ExceptionInfo) {
-		crashlog::initSym();
-		crashlog::loadSym();
-
-		auto info = crashlog::parseException(ExceptionInfo);
+	static LONG WINAPI TopLevelExceptionFilter(struct _EXCEPTION_POINTERS* ExceptionInfo) {
 		std::ostringstream stream;
-		stream << crashlog::exceptionInfoToString(info) << std::endl;
-		stream << std::endl;
-		stream << "StackTrace:" << std::endl;
-		stream << crashlog::createStackTraceString(info) << std::endl;
-		stream << std::endl;
+
+		stream << "Unhandled exception\n";
+		stream << "Exception code: 0x" << std::hex << ExceptionInfo->ExceptionRecord->ExceptionCode << "\n";
+		stream << "Address: " << ExceptionInfo->ExceptionRecord->ExceptionAddress << "\n";
 
 		printf("%s\n", stream.str().c_str());
 		MessageBoxA(nullptr, stream.str().c_str(), "Crashed!", MB_OK | MB_ICONERROR);
 
-		return 0;
+		return EXCEPTION_EXECUTE_HANDLER;
 	}
 
 public:
